10e2488f805a332847b54ce7560d49dbddb76af2
minjko <komj36@gmail.com>
2025-02-11 01:57:52 +0900

    :zap: feat: create Comment CRUD API

--- src/main/java/ce3/wbc/WbcApplication.java
+++ src/main/java/ce3/wbc/WbcApplication.java
@@ -2,7 +2,13 @@ package ce3.wbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
+
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class WbcApplication {
 
