eff3ed1b8590d30461ecb69bfb4bb28124ef33f2
linkpond <linkpond0629@gmail.com>
2025-02-10 10:59:02 +0900

    :pencil2: fix: fix chefs-read

--- src/main/java/ce3/wbc/service/ChefService.java
+++ src/main/java/ce3/wbc/service/ChefService.java
@@ -1,5 +1,6 @@
 package ce3.wbc.service;
 
+import ce3.wbc.controller.rto.response.ChefRes;
 import ce3.wbc.dto.ChefDto;
 import ce3.wbc.entity.Chef;
 import ce3.wbc.repository.ChefRepository;
@@ -15,19 +16,12 @@ public class ChefService {
     @Autowired
     private ChefRepository chefRepository;
 
-    public List<ChefDto> getAllChefs() {
+    public List<ChefRes> getAllChefs() {
         return chefRepository.findAll().stream()
-                .map(this::toDto)
+                .map(ChefRes::toResponse)
                 .collect(Collectors.toList());
     }
 
-    private ChefDto toDto(Chef chef) {
-        ChefDto dto = new ChefDto();
-        dto.setChefId(chef.getChefId());
-        dto.setChefName(chef.getChefName());
-        dto.setChefCategory(chef.getChefCategory());
-        dto.setChefImage(chef.getChefImage());
-        return dto;
-    }
+
 
 }
