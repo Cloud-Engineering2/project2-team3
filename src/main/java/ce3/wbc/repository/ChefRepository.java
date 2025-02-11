7a4c0654b7dcac87d91d42210b4522e73ca2db17
loophyun <dhkim960511@naver.com>
2025-02-11 13:30:47 +0900

    :zap: feat: Add get by id method

--- src/main/java/ce3/wbc/repository/ChefRepository.java
+++ src/main/java/ce3/wbc/repository/ChefRepository.java
@@ -3,6 +3,8 @@ package ce3.wbc.repository;
 
 import ce3.wbc.entity.Chef;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 
