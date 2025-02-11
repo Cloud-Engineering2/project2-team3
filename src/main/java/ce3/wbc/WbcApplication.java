e22dfe8ad507428cba46a1df74242ddd3b6d0821
minjko <komj36@gmail.com>
2025-02-11 10:01:47 +0900

    :art: refactor: remove needless s3 library

--- src/main/java/ce3/wbc/WbcApplication.java
+++ src/main/java/ce3/wbc/WbcApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
-
-
 @EnableJpaAuditing
 @SpringBootApplication
 public class WbcApplication {
